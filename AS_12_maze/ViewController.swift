b60f956b11022f9d6518ca5f2a3f1b2f1f8071f1
AtsushiHosaka <hosaka2917@gmail.com>
2022-05-13 12:05:57 +0900

    Initial Commit

---
+++ AS_12_maze/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AS_12_maze
+//
+//  Created by 保坂篤志 on 2022/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
